f585d7d9a25e59fccd2aed1affef34aa018f894d
Phil Hawksworth <phil@netlify.com>
2021-01-21 10:38:33 +0000

    Add termplate info option

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const inquirer = require('inquirer');
 
 // Assemble a list of template options
 const templatesRoot = `${__dirname}/templates`
-let options = [];
+let templates = [];
 const templateDirs = fs.readdirSync(templatesRoot);
 templateDirs.forEach(folder => {
   let package = require(`${templatesRoot}/${folder}/package.json`);
-  options.push({
+  templates.push({
     "name": `${package.name} (v${package.version})`,
+    "url": package.repository.url.replace("git://", "https://"),
+    "description": package.description,
+    "version": package.version,
     "value": folder
   });
 });
+
+
+// do the right thing
+const argument = process.argv[2];
+const listFlags = ["l", "ls", "list"]
+if(listFlags.includes(argument)) {
+  templateDetails();
+  return;
+}
 
 console.log('');
 console.log(`🎈 ${chalk.green("11up will create a new Eleventy website...")}`);
@@ -23,43 +35,48 @@ console.log('');
 inquirer
   .prompt([
     {
-      type: 'list',
-      name: 'template',
-      message: 'Choose a template',
-      choices: options,
-      filter: function (val) {
-        return val.toLowerCase();
-      },
-    },
-    {
       type: 'input',
       name: 'working_dir',
-      message: "Specify a directory",
+      message: "Specify a directory for your new site",
       default: function () {
         return '.';
       },
     },
+    {
+      type: 'list',
+      name: 'template',
+      message: 'Choose a template',
+      choices: templates.concat({
+        "name": "(or show more info about the templates instead)",
+        "value": "list"
+      }),
+      filter: function (val) {
+        return val.toLowerCase();
+      },
+    },
   ])
   .then((answers) => {
-    copyTemplate(answers.template, answers.working_dir);
+    if(answers.template == "list") {
+      templateDetails();
+      return;
+    } else {
+      copyTemplate(answers.template, answers.working_dir);
+    }
   });
 
 
+// Put things where they belong
 async function copyTemplate(template, dir) {
-
   const targetDir =`${process.cwd()}/${dir}`
   const greenCheck = chalk.green("✓");
-
   console.log('');
   console.log(chalk.green(`✨ Creating a new Eleventy website in ${process.cwd()}/${dir}`));
   console.log('');
   await fs.ensureDir(targetDir);
-
   await fs.copy(`${templatesRoot}/${template}`, targetDir);
   console.log(`${greenCheck} template files copied`);
   console.log(`${greenCheck} package.json copied`);
   console.log(`${greenCheck} .gitignore copied`);
-
   console.log(chalk.green("✓ Done-zo!"));
   console.log('');
   console.log(`To get started...`);
@@ -68,3 +85,18 @@ async function copyTemplate(template, dir) {
   console.log(`👉 npm i`);
   console.log(`👉 npm start`);
 };
+
+
+// Show information about the available templates
+function templateDetails() {
+  console.log('');
+  console.log(chalk.green("Available template details:"), chalk.grey("(npx 11up list)"));
+  templates.forEach(template => {
+    console.log("");
+    console.log(`🎈 ${chalk.green(template.name)}`);
+    console.log(template.description);
+    console.log(template.url);
+  });
+}; 
+
+
